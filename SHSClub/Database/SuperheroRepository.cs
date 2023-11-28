985ad2994a91cfe2e2dcd64aa3de380a314c6533
Gustav-Frising <frising96@gmail.com>
2023-11-28 10:13:41 +0100

    repository

--- SHSClub/Database/SuperheroRepository.cs
+++ SHSClub/Database/SuperheroRepository.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SHSClub.Models;
 
 namespace SHSClub.Database
 {
+
     internal class SuperheroRepository
     {
+        private readonly SHSClubDbContext _context;
+        public SuperheroRepository(SHSClubDbContext context)
+        {
+            _context = context;
+        }
+        public List<SuperheroModel> GetAll()
+        {
+            return _context.Superheroes.ToList();
+
+        }
+
+        public SuperheroModel? GetById(int id)
+        {
+            return _context.Superheroes.FirstOrDefault(p => p.SuperheroId == id);
+        }
+
+        public void Add(SuperheroModel superheromodel)
+        {
+            _context.Superheroes.Add(superheromodel);
+        }
+        public void Update(int id, SuperheroModel superheromodel)
+        {
+            SuperheroModel? superheroToUpdate = GetById(id);
+
+            if (superheroToUpdate != null)
+            {
+                superheroToUpdate.Name = superheromodel.Name;
+                superheroToUpdate.SecretIdentity = superheromodel.SecretIdentity;
+                superheroToUpdate.ImageUrl = superheromodel.ImageUrl;
+                superheroToUpdate.SuperpowerId = superheromodel.SuperpowerId;
+            }
+        }
+        public void Delete(int id)
+        {
+            SuperheroModel? superheroToDelete = GetById(id);
+
+            if (superheroToDelete != null)
+            {
+                _context.Superheroes.Remove(superheroToDelete);
+            }
+        }
+        public void Complete()
+        {
+            _context.SaveChanges();
+            //_context.Dispose();
+        }
     }
 }
---
+++ SHSClub/Database/SuperpowerRepository.cs
@@ -0,0 +1,49 @@
+﻿using SHSClub.Models;
+
+namespace SHSClub.Database
+{
+    internal class SuperpowerRepository
+    {
+        private readonly SHSClubDbContext _context;
+        public SuperpowerRepository(SHSClubDbContext context)
+        {
+            _context = context;
+        }
+        public List<SuperpowerModel> GetAll()
+        {
+            return _context.Superpowers.ToList();
+
+        }
+
+        public SuperpowerModel? GetById(int id)
+        {
+            return _context.Superpowers.FirstOrDefault(p => p.SuperpowerId == id);
+        }
+
+        public void Add(SuperpowerModel superpowermodel)
+        {
+            _context.Superpowers.Add(superpowermodel);
+        }
+        public void Update(int id, SuperpowerModel superpowermodel)
+        {
+            SuperpowerModel? superpowerToUpdate = GetById(id);
+
+            if (superpowerToUpdate != null)
+            {
+                superpowerToUpdate.SuperpowerId = superpowermodel.SuperpowerId;
+                superpowerToUpdate.Name = superpowermodel.Name;
+                superpowerToUpdate.Description = superpowermodel.Description;
+
+            }
+        }
+        public void Delete(int id)
+        {
+            SuperpowerModel? superpowerToDelete = GetById(id);
+
+            if (superpowerToDelete != null)
+            {
+                _context.Superpowers.Remove(superpowerToDelete);
+            }
+        }
+    }
+}
